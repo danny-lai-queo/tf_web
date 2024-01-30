ed74a614b5b92bf85d63be12a0ef0d4fb375c291
danny-lai-queo <danny.lai@queosystems.com>
2024-01-29 19:42:15 -0800

    use tensorflowLite, img base64, delete uploaded files

--- main.py
+++ main.py
@@ -6,12 +6,44 @@ import numpy as np
 
 from flask_cors import CORS, cross_origin
 
-names = ["daisy", "dandelon", "roses", "sunflowers", "tulips"]
+import tensorflow as tf
+
+from datetime import datetime
+
+import base64
+import os
+
+#names = ["daisy", "dandelon", "roses", "sunflowers", "tulips"]
+class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
 
 
 
 # Process image and predict label
-def processImg(IMG_PATH):
+def processImg(img_path):
+    img_height = 180
+    img_width = 180
+    img = tf.keras.utils.load_img(img_path, target_size=(img_height, img_width))
+
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    TF_MODEL_FILE_PATH = 'model.tflite' # The default path to the saved TensorFlow Lite model
+    interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
+    sig_dict = interpreter.get_signature_list()
+    print(f"sig_dict = {sig_dict}")
+    sig = list(sig_dict)[0]
+    print(f'sig = {sig}')
+    classify_lite = interpreter.get_signature_runner('serving_default')
+    print(classify_lite)
+    predictions_lite = classify_lite(sequential_1_input=img_array)['outputs']
+    score_lite = tf.nn.softmax(predictions_lite)
+    label_name = class_names[np.argmax(score_lite)]
+    confidence_percent = 100 * np.max(score_lite)
+    print("This image most likely belongs to {} with a {:.2f} percent confidence.".format(label_name, confidence_percent))
+
+    return label_name, confidence_percent
+
+def processImg_old(IMG_PATH):
     # Read image
     model = load_model("flower.model")
     
@@ -25,7 +57,7 @@ def processImg(IMG_PATH):
     res = model.predict(image)
     label = np.argmax(res)
     print("Label", label)
-    labelName = names[label]
+    labelName = class_names[label]
     print("Label name:", labelName)
     return labelName
 
@@ -50,13 +82,22 @@ def postsPage():
 @app.route("/process", methods=["POST"])
 def processReq():
     data = request.files["fileToUpload"]
-    data.save("img.jpg")
+    new_filepath = datetime.utcnow().strftime('%Y%m%d_%H%M%S_%f')
+    # data.save("img.jpg")
+    data.save(new_filepath)
 
-    flower_name = processImg("img.jpg")
+    with open(new_filepath, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
+    #flower_name, confidence_percent = processImg("img.jpg")
+    flower_name, confidence_percent = processImg(new_filepath)
+
+    confidence_percent_str = "{:.2f}".format(confidence_percent)
+
+    os.remove(new_filepath)
 
     #return flower_name
-    return render_template("response.html", flower_name=flower_name)
+    return render_template("response.html", flower_name=flower_name, confidence_percent_str=confidence_percent_str, img_base64_str=encoded_string)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -7,8 +7,8 @@ your localhost url. If running on port 5000
 url = "http://localhost:5000/process"
 # Path to image file
 #filess = {"img": open("PATH_TO_INPUT_IMAGE", "rb")}
-filess = {"img": open("img.jpg", "rb")}
+files = {"fileToUpload": open("img.jpg", "rb")}
 starttime = time.time()
-results = requests.post(url, files=filess)
+results = requests.post(url, files=files)
 print("time taken:", time.time() - starttime)
 print(results.text)
